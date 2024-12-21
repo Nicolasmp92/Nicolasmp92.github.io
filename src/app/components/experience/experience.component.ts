178fe8f968ae2102f582c28a309ea2b7e79d4fad
Nicolas Muñoz <nikolasmp92@gmail.com>
2024-12-21 18:21:01 -0300

    cambio de background, agregando proyectos a portafolio

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -27,10 +27,60 @@ export class ExperienceComponent {
   @ViewChild('dialogTemplate', { static: true }) dialogTemplate!: TemplateRef<any>;
 
   projects = [
-    { title: 'Recos.cl', description: 'Constructora.', image: 'img/header_Recos.png', fullimage:'img/Recosfull.png', details: 'WEB SPA informativa, para proporcionar mas informacion a los clientes y captar nuevas oportunidades de negocios.' },
-    { title: 'colegioangeles.cl', description: 'Colegio Especial Ángeles.', image: 'img/header_Angeles.png',fullimage:'img/Angelesfull.png', details: 'Información detallada del proyecto 2.' },
-    { title: 'Proyecto 3', description: 'Descripción breve del proyecto 3.', image: 'https://via.placeholder.com/150', details: 'Información detallada del proyecto 3.' }
+    {
+      title: 'Recos.cl',
+      description: 'Constructora.',
+      image: 'img/header_Recos.png',
+      fullimage: 'img/Recosfull.png',
+      details: `
+        <p>WEB SPA informativa para proporcionar más información a los clientes y captar nuevas oportunidades de negocios.</p>
+        <ul>
+          <li>Slider de imágenes principal.</li>
+          <li>Tarjetas descriptivas de misión, visión y valores.</li>
+          <li>Sección del equipo con descripción e imagen representativa.</li>
+          <li>Galería de proyectos con miniaturas y detalles.</li>
+          <li>Logotipos de clientes destacados.</li>
+          <li>Formulario de contacto funcional con campos de nombre, teléfono, correo y mensaje.</li>
+          <li>Botones para redes sociales en el pie de página.</li>
+        </ul>
+      `,
+    },
+    {
+      title: 'colegioangeles.cl',
+      description: 'Colegio Ángeles.',
+      image: 'img/header_Angeles.png',
+      fullimage: 'img/Angelesfull.png',
+      details: `
+        <p>Información detallada del proyecto 2.</p>
+        <ul>
+          <li>Slider principal.</li>
+          <li>Información institucional destacada.</li>
+          <li>Galería de imágenes del colegio.</li>
+          <li>Formulario de contacto con campos de texto.</li>
+          <li>Botones para redes sociales.</li>
+        </ul>
+      `,
+    },
+    {
+      title: 'Proyecto 3',
+      description: 'Descripción breve del proyecto 3.',
+      image: 'https://via.placeholder.com/150',
+      details: `
+        <p>Información detallada del proyecto 3.</p>
+        <ul>
+          <li>Información del proyecto en formato SPA.</li>
+          <li>Galería de imágenes representativas.</li>
+          <li>Botón de contacto.</li>
+        </ul>
+      `,
+    },
   ];
+  habilidadesFront = [
+    { icono: 'iconos/angular.ico', nombre: 'Angular', progreso: 80 },
+    { icono: 'iconos/js.ico', nombre: 'JavaScript', progreso: 80 },
+    { icono: 'iconos/html5.ico', nombre: 'HTML5', progreso: 80 },
+    { icono: 'iconos/css3.ico', nombre: 'CSS3', progreso: 80 },
+    { icono: 'iconos/bootstrap.ico', nombre: 'Bootstrap5ñ', progreso: 80 },];
 
   experiences = [
     {
