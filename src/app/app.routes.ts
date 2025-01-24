232d2e586e18fa91712e63c9edd62b0f75e7c475
Nicolas Muñoz <nikolasmp92@gmail.com>
2025-01-24 19:36:00 -0300

    prueba contact.ts 5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,18 +7,15 @@ import { SkillsComponent } from './components/skills/skills.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ExperienceComponent } from './components/experience/experience.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  // Redirige la raíz a home
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent},
-  { path: 'experiencia', component: ExperienceComponent},
-  { path: 'habilidades', component: SkillsComponent},
-  { path: 'contacto', component: ContactComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'experiencia', component: ExperienceComponent },
+  { path: 'habilidades', component: SkillsComponent },
+  { path: 'contacto', component: ContactComponent },
 
-
-  // ... otras rutas que puedas tener en el futuro
-  { path: '**', component: NotFoundComponent } // Ruta comodín para manejar errores 404 (opcional)
+  // Ruta comodín para manejar errores 404 (opcional)
+  { path: '**', component: NotFoundComponent }
 ];
